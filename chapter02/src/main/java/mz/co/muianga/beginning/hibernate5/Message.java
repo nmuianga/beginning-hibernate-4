4ca1ac70f422904fca6726906e2455660b1e42d2
= <=>
2019-10-03 10:48:49 +0200

    Fim do Capítulo 02

--- chapter02/src/main/java/mz/co/muianga/beginning/hibernate5/Message.java
+++ chapter02/src/main/java/mz/co/muianga/beginning/hibernate5/Message.java
@@ -16,7 +16,7 @@ import javax.persistence.Id;
 @Entity
 public class Message {
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
 	
 	@Column(nullable = false)
